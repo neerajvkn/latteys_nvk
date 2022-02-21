785dfe438cf1d766558057699ff7085c9cf4f3cb
Neeraj VK <neerajvkn@gmail.com>
2022-02-21 12:57:02 +0000

    created final purchase order planning report

---
+++ latteys_nvk/latteys_nvk/report/final_purchase_order_planning/final_purchase_order_planning.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Neeraj VK and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Final Purchase Order Planning"] = {
+	"filters": [
+
+	]
+};
---
+++ latteys_nvk/latteys_nvk/report/final_purchase_order_planning/final_purchase_order_planning.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Neeraj VK and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
